512729884d6a61140f259b063bd60c90867ea55c
Nikunj Panchal <nikunj@indianic.com>
2022-03-30 21:30:47 +0530

    Cronjob changes for sending an alert email to users for their growthmodel activities

--- growthmodel/tasks.py
+++ growthmodel/tasks.py
@@ -5,30 +5,39 @@ from django.utils import timezone
 from datetime import timedelta
 from django.db.models import Q
 from django.template.loader import render_to_string
+from rtf.celery import app
 
 @shared_task
 def send_growthmodel_activity_alert_email():
     start_date = timezone.now() + timedelta(days=1)
     end_date = timezone.now() + timedelta(days=1)
 
-    growthmodel_ids = list(GrowthModelActivity.objects.exclude(Q(alert=False) | 
+    growthModelActivityObjs = GrowthModelActivity.objects.exclude(Q(alert=False) | 
         Q(start_date__isnull=True) | Q(end_date__isnull=True)).filter(
-        end_date__range=(start_date, end_date)).values_list('growth_model_id', flat=True))
+        end_date__range=(start_date, end_date))
 
-    growthmodelObjs = GrowthModel.objects.exclude(user_id__isnull=True)
+    growthModelObjs = GrowthModel.objects.exclude(user_id__isnull=True)
+    userObjs = Users.objects.exclude(email__isnull=True)
 
-    for growthmodel_id in growthmodel_ids:
-        userObj = Users.objects.filter(id=growthmodelObjs.get(
-            id=growthmodel_id).user_id)
+    for growthModelActivityObj in growthModelActivityObjs:
+        userObj = userObjs.filter(id=growthModelObjs.get(
+            id=growthModelActivityObj.growthmodel_id).user_id)
 
         # Send an activity alert email
         subject = "Growth Model Actvity Alert"
         message_body = render_to_string(
-            'growthmodel/growth_model_activity_alert.html', {})
+            'growthmodel/growth_model_activity_alert.html', {
+                'activity_type': activity_type,
+                'activity_title': activity_title,
+            }
+        )
         from_email = ""
         recipient_list = [userObj.email,]
         send_an_email(subject, message_body, recipient_list, from_email)
 
     return True
 
-            
+@app.task
+def add(x, y):
+    z = x + y
+    print(z)
--- rtf/celery.py
+++ rtf/celery.py
@@ -1,10 +1,21 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # setting the Django settings module.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rtf.settings')
 app = Celery('rtf')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = {
+    # Executes daily at midnight.
+    'add-every-monday-morning': {
+        'task': 'growthmodel.tasks.add',
+        # 'schedule': crontab(hour=0, minute=0),
+        'schedule': crontab(minute='*/1'),
+        'args': (16, 16),
+    },
+}
 
 # Looks up for task modules in Django applications and loads them
 app.autodiscover_tasks()
